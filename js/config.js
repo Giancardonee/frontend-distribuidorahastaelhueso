55422085663efa1485d21cfb1d08c76f2759963c
Gian <giancardonee@gmail.com>
2025-09-14 14:46:52 -0300

    Cambiamos config.js de const a window

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Este archivo contiene configuraciones globales para la aplicación.
 
-const API_BASE_URL = "https://backend-distribuidorahastaelhueso.onrender.com/distribuidora";
+window.API_BASE_URL = "https://backend-distribuidorahastaelhueso.onrender.com/distribuidora";
