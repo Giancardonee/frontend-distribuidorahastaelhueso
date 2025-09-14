0f8397f7efcd7eb08ea0e2c8183f6a2dcf1abec0
Gian <giancardonee@gmail.com>
2025-09-14 14:53:56 -0300

    Corrigiendo url realizarVenta.js

--- js/realizarVenta/scriptRealizarVenta.js
+++ js/realizarVenta/scriptRealizarVenta.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- URLs Base ---
 
-    const BASE_URL = "http://localhost:8080";
+    const BASE_URL = window.API_BASE_URL; 
 
     const API_VENTAS_URL = `${BASE_URL}/distribuidora/ventas`;
     const API_CLIENTES_URL = `${BASE_URL}/distribuidora/clientes`;
